53157d0a25b6c147e2b906d31d3a9879c49ed0d7
Henrique Melo <riquemelo98@gmail.com>
2018-04-07 01:11:58 -0300

    Algumas atualizações de JS

--- main.js
+++ main.js
@@ -1,77 +1,154 @@
-function create(){
+function createMotorista(){
 //Variaveis do motorista
-        var nome = $("#formNome").val();
-        var nasc = $("#formNasc").val();
-        var cpf = $("#formCpf").val();
-        var carro = $("#formCarro").val();
-        var sexo = $("#formSexo").val();
-        var status = $("#formStatus").val();
+var motorista = {};
+        motorista.Nome = $("#formNomeM").val();
+        motorista.Nascimento = $("#formNascM").val();
+        motorista.Cpf = $("#formCpfM").val();
+        motorista.ModeloCarro = $("#formCarroM").val();
+        motorista.Sexo = $("#formSexoM").val();
+        motorista.Status = $("#formStatusM").val();
 //Variavel de controle para apresentação de Erro
-        var error = "";
-        var cerr = 0;
-        submitOK = "true";
+        motorista.MensagemError = "";
+        motorista.ContadorError = 0;
+        motorista.SubmitOK = "true";
 //Validação dos dados
-        if (nome == "") {
-            if(cerr == 0) {error += "nome"; cerr++}
-            else {error += ", nome"; cerr++}
+        if (motorista.Nome == "") {
+            if(motorista.ContadorError == 0) {motorista.MensagemError += "nome"; motorista.ContadorError++}
+            else {motorista.MensagemError += ", nome"; motorista.ContadorError++}
              
             submitOK = "false";
         }
         
-        if (nasc == "" || parseInt(nasc.substr(0,2)) > 31 || 
-        parseInt(nasc.substr(2,2)) > 12 || parseInt(nasc.substr(4,4)) > 2018) {
-            if(cerr == 0) {error += "data de nascimento"; cerr++}
-            else {error += ", data de nascimento"; cerr++}
+        if (motorista.Nascimento == "" || parseInt(motorista.Nascimento.substr(0,2)) > 31 || 
+        parseInt(N.substr(2,2)) > 12 || parseInt(motorista.Nascimento.substr(4,4)) > 2018 ||
+        motorista.Nascimento.length != 8) {
+            if(motorista.ContadorError == 0) {error += "data de nascimento"; motorista.ContadorError++}
+            else {motorista.MensagemError += ", data de nascimento"; motorista.ContadorError++}
             submitOK = "false";
             }
 
-        if (cpf == "") {
-            if(cerr == 0) {error += "CPF"; cerr++}
-            else {error += ", CPF"; cerr++}
+        if (motorista.Cpf == "") {
+            if(motorista.ContadorError == 0) {motorista.MensagemError += "CPF"; motorista.ContadorError++}
+            else {motorista.MensagemError += ", CPF"; motorista.ContadorError++}
             submitOK = "false";
         }
 
-        if (carro == "") {
-            if(cerr == 0) {error += "modelo do carro"; cerr++}
-            else {error += ", modelo do carro"; cerr++}
+        if (motorista.ModeloCarro == "") {
+            if(motorista.ContadorError == 0) {motorista.MensagemError += "modelo do carro"; motorista.ContadorError++}
+            else {motorista.MensagemError += ", modelo do carro"; motorista.ContadorError++}
             submitOK = "false";
         }
         
-        if (sexo != "Masculino" && sexo != "Feminino") {
-            if(cerr == 0) {error += "sexo"; cerr++}
-            else {error += ", sexo"; cerr++}
+        if (motorista.Sexo != "Masculino" && motorista.Sexo != "Feminino") {
+            if(motorista.ContadorError == 0) {motorista.MensagemError += "sexo"; motorista.ContadorError++}
+            else {motorista.MensagemError += ", sexo"; motorista.ContadorError++}
             submitOK = "false";
         }
 
-        if (status != "Ativo" && status != "Inativo") {
-            if(cerr == 0) {error += "status"; cerr++}
-            else {error += ", status"; cerr++}
+        if (motorista.Status != "Ativo" && motorista.Status != "Inativo") {
+            if(motorista.ContadorError == 0) {motorista.MensagemError += "status"; motorista.ContadorError++}
+            else {motorista.MensagemError += ", status"; motorista.ContadorError++}
             submitOK = "false";
         }
         if (submitOK == "false") {
-            if(cerr == 1){$("#formContainer").append('<span style="color: red"> Por favor, preencha corretamente o campo '+ error +'.</span>')}
-            else{$("#formContainer").append('<span style="color: red"> Por favor, preencha corretamente o campo '+ error +'.</span>')}
+            if(motorista.ContadorError == 1){
+                $("#mensagemValidacao").html('<span> Por favor, preencha corretamente o campo '+ motorista.MensagemError +'.</span>')
+            }
+            else{
+                $("#mensagemValidacao").html('<span> Por favor, preencha corretamente o campo '+ motorista.MensagemError +'.</span>')
+            }
             return false;
 
         }else{
-            var table = document.getElementById("tabela");
-            var row = table.insertRow(table.getElementsByTagName("tr").length - 1);
+            motorista.Table = document.getElementById("tabelaMotorista");
+            motorista.Row = motorista.Table.insertRow(motorista.Table.getElementsByTagName("tr").length - 1);
             cells = [
-                row.insertCell(0),
-                row.insertCell(1),
-                row.insertCell(2),
-                row.insertCell(3),
-                row.insertCell(4),
-                row.insertCell(5)
+                motorista.Row.insertCell(0),
+                motorista.Row.insertCell(1),
+                motorista.Row.insertCell(2),
+                motorista.Row.insertCell(3),
+                motorista.Row.insertCell(4),
+                motorista.Row.insertCell(5)
             ];
 
-            cells[0].innerHTML = nome;
-            cells[1].innerHTML = nasc.substr(0,2) + "/" + nasc.substr(2,2) + "/" + nasc.substr(4,4); 
-            cells[2].innerHTML = cpf.substr(0,3) + "." + cpf.substr(3,3) + "." + cpf.substr(6,3) + "-" + cpf.substr(9,2);
-            cells[3].innerHTML = carro;
-            cells[4].innerHTML = sexo;
-            cells[5].innerHTML = status;
+            cells[0].innerHTML = motorista.Nome;
+            cells[1].innerHTML = motorista.Nascimento.substr(0,2) + "/" + motorista.Nascimento.substr(2,2) + "/" + motorista.Nascimento.substr(4,4); 
+            cells[2].innerHTML = motorista.Cpf.substr(0,3) + "." + motorista.Cpf.substr(3,3) + "." + motorista.Cpf.substr(6,3) + "-" + motorista.Cpf.substr(9,2);
+            cells[3].innerHTML = motorista.ModeloCarro;
+            cells[4].innerHTML = motorista.Sexo;
+            cells[5].innerHTML = motorista.Status;
         }
         $('#adicionarMotorista').modal('toggle');
         
 }
+function createPassageiro(){
+//Variaveis do passageiro
+passageiro = {};
+            passageiro.Nome = $("#formNomeP").val();
+            passageiro.Nascimento = $("#formNascP").val();
+            passageiro.Cpf = $("#formCpfP").val();
+            passageiro.Sexo = $("#formSexoP").val();
+//Variavel de controle para apresentação de Erro
+            passageiro.MensagemError = "";
+            passageiro.ContadorError = 0;
+            passageiro.SubmitOK = "true";
+//Validação dos dados
+            if (passageiro.Nome == "") {
+                if(passageiro.ContadorError == 0) {passageiro.MensagemError += "nome"; passageiro.ContadorError++}
+                else {passageiro.MensagemError += ", nome"; passageiro.ContadorError++}
+                 
+                passageiro.SubmitOK = "false";
+            }
+            
+            if (passageiro.Nascimento == "" || parseInt(passageiro.Nascimento.substr(0,2)) > 31 || 
+            parseInt(passageiro.Nascimento.substr(2,2)) > 12 || parseInt(passageiro.Nascimento.substr(4,4)) > 2018 ||
+            passageiro.Nascimento.length != 8) {
+                if(passageiro.ContadorError == 0) {passageiro.MensagemError += "data de nascimento"; passageiro.ContadorError++}
+                else {passageiro.MensagemError += ", data de nascimento"; passageiro.ContadorError++}
+                passageiro.SubmitOK = "false";
+                }
+    
+            if (passageiro.Cpf == "") {
+                if(passageiro.ContadorError == 0) {passageiro.MensagemError += "CPF"; passageiro.ContadorError++}
+                else {passageiro.MensagemError += ", CPF"; passageiro.ContadorError++}
+                passageiro.SubmitOK = "false";
+            }
+            
+            if (passageiro.Sexo != "Masculino" && passageiro.Sexo != "Feminino") {
+                if(passageiro.ContadorError == 0) {passageiro.MensagemError += "sexo"; passageiro.ContadorError++}
+                else {passageiro.MensagemError += ", sexo"; passageiro.ContadorError++}
+                passageiro.SubmitOK = "false";
+            }
+//Mensagem de Erro    
+            if (passageiro.SubmitOK == "false") {
+                if(passageiro.ContadorError == 1){
+                    $("#mensagemValidacao").html('<span> Por favor, preencha corretamente o campo '+ passageiro.MensagemError +'.</span>');
+                }
+                else{
+                    $("#mensagemValidacao").html('<span> Por favor, preencha corretamente o campo '+ passageiro.MensagemError +'.</span>');
+                }
+                return false;
+    
+            }else{
+                passageiro.Table = document.getElementById("tabelaPassageiro");
+                passageiro.Row = passageiro.Table.insertRow(passageiro.Table.getElementsByTagName("tr").length - 1);
+                cells = [
+                    row.insertCell(0),
+                    row.insertCell(1),
+                    row.insertCell(2),
+                    row.insertCell(3),
+                    row.insertCell(4),
+                    row.insertCell(5)
+                ];
+    
+                cells[0].innerHTML = passageiro.Nome;
+                cells[1].innerHTML = passageiro.Nascimento.substr(0,2) + "/" + passageiro.Nascimento.substr(2,2) + "/" + passageiro.Nascimento.substr(4,4); 
+                cells[2].innerHTML = passageiro.Cpf.substr(0,3) + "." + passageiro.Cpf.substr(3,3) + "." + passageiro.Cpf.substr(6,3) + "-" + passageiro.Cpf.substr(9,2);
+                cells[3].innerHTML = passageiro.ModeloCarro;
+                cells[4].innerHTML = passageiro.Sexo;
+                cells[5].innerHTML = passageiro.Status;
+            }
+            $('#adicionarPassageiro').modal('toggle');
+            
+    }
+    
